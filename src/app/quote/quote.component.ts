77cac584eabc62c34d728976274fff44fc4adcac
BTG001 <brunokipkorir2@gmail.com>
2020-06-14 19:25:40 +0300

    moved the quote array permanently from app component to quote component class

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote'
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+  quotes:Quote[] = [
+    {name:"To bring about change, you must not be afraid to take the first step. We will fail when we fail to try", author:"Rosa Parks"},
+    {name:"Not everything that is faced can be changed, but nothing can be changed until it is faced", author:"James Baldwin"},
+    {name:"An eye for an eye makes the world blind", author:"Mahatma Gandhi"},
+    {name:"It doesn't matter who you are, where you come from. The ability to triumph begins with you — always", author:"Oprah Winfrey"},
+    {name:"Darkness cannot drive out darkness; only light can do that. Hate cannot drive out hate; only love can do that", author:"Martin Luther King Jr."},
+  ];
+
 
   constructor() { }
 
