7563a062c6463c4977528a262887a94bfb8acdee
BTG001 <brunokipkorir2@gmail.com>
2020-06-14 16:49:22 +0300

    Import Qoute blueprint class and Inside the AppComponent class, we have created an object quotes and attributed it to the Quote blueprint and then defined the array of quotes each with its name and author.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
-
+import { Quote } from './quote'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quotes: string[];
-  constructor() {
-    this.quotes = ["To bring about change, you must not be afraid to take the first step. We will fail when we fail to try", "Not everything that is faced can be changed, but nothing can be changed until it is faced", "An eye for an eye makes the world blind"]
-  }
+  quotes:Quote[] = [
+    {name:"To bring about change, you must not be afraid to take the first step. We will fail when we fail to try", author:"Rosa Parks"},
+    {name:"Not everything that is faced can be changed, but nothing can be changed until it is faced", author:"James Baldwin"},
+    {name:"An eye for an eye makes the world blind", author:"Mahatma Gandhi"},
+    {name:"It doesn't matter who you are, where you come from. The ability to triumph begins with you — always", author:"Oprah Winfrey"},
+    {name:"Darkness cannot drive out darkness; only light can do that. Hate cannot drive out hate; only love can do that", author:"Martin Luther King Jr."},
+  ];
 }
