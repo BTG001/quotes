be26fdd373398e93421857a38394add3c6fd660d
BTG001 <brunokipkorir2@gmail.com>
2020-06-14 16:07:30 +0300

    Create property quotes and attributed it to an empty string. Create constructor function and give the quotes array 3 values so that it's no longer empty

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quote = "To bring about change, you must not be afraid to take the first step. We will fail when we fail to try"
+  quotes: string[];
+  constructor() {
+    this.quotes = ["To bring about change, you must not be afraid to take the first step. We will fail when we fail to try", "Not everything that is faced can be changed, but nothing can be changed until it is faced", "An eye for an eye makes the world blind"]
+  }
 }
