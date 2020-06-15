d6d92039cfcec8626d4d74f249e4aaebd7496eec
BTG001 <brunokipkorir2@gmail.com>
2020-06-15 03:05:49 +0300

    add upvote, downvote and date in the quote blueprint

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,4 +1,10 @@
 export class Quote {
-  name: string;
-  author: string;
+  public upvotes:number
+  public downvotes:number
+  public myDate:Date
+  constructor(public name:string, public author:string, public quote:string){
+    this.upvotes=0
+    this.downvotes=0
+    this.myDate= new Date()
+    }
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,11 +9,11 @@ import { Quote } from '../quote'
 export class QuoteComponent implements OnInit {
 
   quotes:Quote[] = [
-    {name:"To bring about change, you must not be afraid to take the first step. We will fail when we fail to try", author:"Rosa Parks"},
-    {name:"Not everything that is faced can be changed, but nothing can be changed until it is faced", author:"James Baldwin"},
-    {name:"An eye for an eye makes the world blind", author:"Mahatma Gandhi"},
-    {name:"It doesn't matter who you are, where you come from. The ability to triumph begins with you — always", author:"Oprah Winfrey"},
-    {name:"Darkness cannot drive out darkness; only light can do that. Hate cannot drive out hate; only love can do that", author:"Martin Luther King Jr."},
+    {name:"To bring about change, you must not be afraid to take the first step. We will fail when we fail to try.", author:"Rosa Parks"},
+    {name:"Not everything that is faced can be changed, but nothing can be changed until it is faced.", author:"James Baldwin"},
+    {name:"An eye for an eye makes the world blind.", author:"Mahatma Gandhi"},
+    {name:"It doesn't matter who you are, where you come from. The ability to triumph begins with you — always.", author:"Oprah Winfrey"},
+    {name:"Darkness cannot drive out darkness; only light can do that. Hate cannot drive out hate; only love can do that.", author:"Martin Luther King Jr."},
   ];
 
 
